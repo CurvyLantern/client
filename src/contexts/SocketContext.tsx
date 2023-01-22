d8bdc0078de157a435179b95b980af1b21147b31
CurvyLantern <ashfaqnasim33@gmail.com>
2023-01-22 16:27:24 +0600

    env

--- src/contexts/SocketContext.tsx
+++ src/contexts/SocketContext.tsx
@@ -2,7 +2,7 @@ import React, { createContext, useEffect, useRef, useState } from 'react';
 
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:8000');
+const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:8000');
 
 const SocketContext = createContext<{
 	socket: ReturnType<typeof io> | null;
