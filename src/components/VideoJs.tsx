315ad93dc7ce80b30090908403ed94feefbb7aec
CurvyLantern <ashfaqnasim33@gmail.com>
2023-01-24 15:50:14 +0600

    useeffect dependency updated

--- src/components/VideoJs.tsx
+++ src/components/VideoJs.tsx
@@ -38,7 +38,7 @@ export const VideoJS = ({
 				player.src(options.sources);
 			}
 		}
-	}, [options, videoRef]);
+	}, [options, videoRef, onReady]);
 
 	// Dispose the Video.js player when the functional component unmounts
 	React.useEffect(() => {
