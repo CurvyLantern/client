2a200630a3b7661340f3187f8a65fdb77b22e638
Tuhin Hossain <95519628+tuhin180@users.noreply.github.com>
2023-03-25 20:29:07 +0600

    namaje jai

--- src/pages/Tuhin/index.tsx
+++ src/pages/Tuhin/index.tsx
@@ -1,10 +1,15 @@
-const user = [1, 2];
+const user = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
 const TuhinPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <div className="mx-auto my-10 grid max-w-7xl grid-cols-1 gap-14 md:grid-cols-2 lg:grid-cols-4">
+    <div className="m-4 flex min-h-screen flex-row items-center justify-center gap-5 overflow-hidden">
+      <div
+        className={` flex ${
+          user.length === 1 ? "w-1/2" : "w-full "
+        }  gap-5 md:grid-cols-2 lg:grid-cols-3`}
+      >
         {user.map((item) => (
-          <video key={item} src="" className="w-full" controls></video>
+          <video key={item} className={`w-full `} src="" controls></video>
         ))}
       </div>
     </div>
