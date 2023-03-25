27b4147d9dc4d855a2057c2fadf61a544fc270b9
Tuhin Hossain <95519628+tuhin180@users.noreply.github.com>
2023-03-26 00:58:23 +0600

    initial done

---
+++ src/components/tuhin/main.tsx
@@ -0,0 +1,9 @@
+const Screen = () => {
+  return (
+    <div className="flex h-full flex-col items-center justify-center">
+      <video src="" autoPlay controls></video>
+    </div>
+  );
+};
+
+export default Screen;
--- src/pages/Tuhin/index.tsx
+++ src/pages/Tuhin/index.tsx
@@ -1,19 +1,37 @@
-const user = [1, 2, 3];
+// const user = [1, 2, 3, 4, 5, 6, 7, 8];
 
-const TuhinPage = () => {
+import Footer from "@/components/tuhin/Footer";
+import Screen from "@/components/tuhin/main";
+
+// const TuhinPage = () => {
+//   return (
+//     <div className="m-4 flex min-h-screen flex-col  items-center justify-center gap-5 overflow-hidden">
+//       <div
+//         className={` flex ${
+//           user.length === 1 ? "w-1/2" : "w-full "
+//         }  gap-5 md:grid-cols-2 lg:grid-cols-3`}
+//       >
+//         {user.map((item) => (
+//           <video key={item} className={`w-full `} src="" controls></video>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default TuhinPage;
+
+const index = () => {
   return (
-    <div className="m-4 flex min-h-screen flex-col  items-center justify-center gap-5 overflow-hidden">
-      <div
-        className={` flex ${
-          user.length === 1 ? "w-1/2" : "w-full "
-        }  gap-5 md:grid-cols-2 lg:grid-cols-3`}
-      >
-        {user.map((item) => (
-          <video key={item} className={`w-full `} src="" controls></video>
-        ))}
+    <div className="w-full">
+      <div className="h-[80vh] w-full ">
+        <Screen />
+      </div>
+      <div className="h-[20vh]">
+        <Footer />
       </div>
     </div>
   );
 };
 
-export default TuhinPage;
+export default index;
